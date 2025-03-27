9448a81d76f2c4ab2495e8a6ac90c476918359c2
Gonzalo JunLor <gonzalo.junlor@educa.jcyl.es>
2025-03-27 23:45:21 +0100

    01

--- 01/js/script.js
+++ 01/js/script.js
@@ -7,16 +7,34 @@ parrafo[0].innerText = "Parrafo 1 modificado"
 parrafo[1].innerText = "Parrafo 2 modificado"
 parrafo[2].innerText = "Parrafo 3 modificado"
 
-parrafo[0].outerHTML = "<div>Parrafo 11 modificado</div>"
+//parrafo[0].outerHTML = "<div>Parrafo 11 modificado</div>"
 
 cuerpo.style.backgroundColor="#f4f4f4"
 cuerpo.style.textAlign="center"
 
 titulo.style.color="#333"
-titulo.style.textTransform="uppercase"
 titulo.style.fontWeight="bold"
 titulo.style.marginBottom="15px"
+titulo.style.textTransform="uppercase"
 
-array.forEach(p => {
-    p.style.color="#e74c3c"
-});
+for (var i = 0; i < parrafo.length; i++) {
+    parrafo[i].style.fontSize="18px"
+    parrafo[i].style.padding="10px"
+    parrafo[i].style.width="60%"
+    parrafo[i].style.borderLeft="solid 5px"
+    parrafo[i].style.borderRadius = "5px"
+    parrafo[i].style.lineHeight="1.6"
+    parrafo[i].style.margin = "10px auto"
+}
+
+parrafo[0].style.color="#e74c3c"
+parrafo[0].style.backgroundColor="#ffe6e6"
+parrafo[0].style.borderColor="#c0392b"
+
+parrafo[1].style.color="#2ecc71"
+parrafo[1].style.backgroundColor="#eaffea"
+parrafo[1].style.borderColor="#27ea60"
+
+parrafo[2].style.color="#3498db"
+parrafo[2].style.backgroundColor="#e6f2ff"
+parrafo[2].style.borderColor="#2980b9"
